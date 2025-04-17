a19493e7108793ce35c16c1212251f17276036ba
Sumorio <95837074+Sumorio@users.noreply.github.com>
2025-04-17 14:39:01 +0800

    Update fclite.js
    隐藏作者信息

--- main/fclite.js
+++ main/fclite.js
@@ -67,12 +67,12 @@ function initialize_fc_lite() {
         allArticles = data.article_data;
         // 处理统计数据
         const stats = data.statistical_data;
-        statsContainer.innerHTML = `
-            <div>Powered by: <a href="https://github.com/willow-god/Friend-Circle-Lite" target="_blank">FriendCircleLite</a><br></div>
-            <div>Designed By: <a href="https://www.liushen.fun/" target="_blank">LiuShen</a><br></div>
-            <div>订阅:${stats.friends_num}   活跃:${stats.active_num}   总文章数:${stats.article_num}<br></div>
-            <div>更新时间:${stats.last_updated_time}</div>
-        `;
+        // statsContainer.innerHTML = `
+        //     <div>Powered by: <a href="https://github.com/willow-god/Friend-Circle-Lite" target="_blank">FriendCircleLite</a><br></div>
+        //     <div>Designed By: <a href="https://www.liushen.fun/" target="_blank">LiuShen</a><br></div>
+        //     <div>订阅:${stats.friends_num}   活跃:${stats.active_num}   总文章数:${stats.article_num}<br></div>
+        //     <div>更新时间:${stats.last_updated_time}</div>
+        // `;
 
         displayRandomArticle(); // 显示随机友链卡片
 
